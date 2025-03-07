2c6480dd379056ff07268630d5869e61239c0d5a
Resplendence <142782002+ResplendentAlex@users.noreply.github.com>
2025-03-07 17:04:01 +0700

    [GREEN] Implement PaymentServiceImpl

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -0,0 +1,39 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import java.util.List;
+import java.util.Map;
+
+import enums.PaymentMethod;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+
+public class PaymentServiceImpl implements PaymentService {
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Override
+    public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
+        if (!PaymentMethod.contains(method)) {
+            throw new IllegalArgumentException();
+        }
+        return paymentRepository.addPayment(order, method, paymentData);
+    }
+
+    @Override
+    public Payment setStatus(Payment payment, String status) {
+        return paymentRepository.setStatus(payment, status);
+    }
+
+    @Override
+    public Payment getPayment(String paymentId) {
+        return paymentRepository.getPayment(paymentId);
+    }
+
+    @Override
+    public List<Payment> getAllPayments() {
+        return paymentRepository.getAllPayments();
+    }
+}
