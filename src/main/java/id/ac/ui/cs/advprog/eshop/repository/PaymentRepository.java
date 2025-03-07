e0f8ee2e57125ecdd5f1b6878d8dd8cba7a82974
Resplendence <142782002+ResplendentAlex@users.noreply.github.com>
2025-03-07 17:00:49 +0700

    [RED] Add PaymentRepository skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,27 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import enums.OrderStatus;
+import enums.PaymentStatus;
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public class PaymentRepository {
+    private List<Payment> paymentData;
+
+    public PaymentRepository() {
+        this.paymentData = new ArrayList<>();
+    }
+
+    public Payment addPayment(Order order, String method, Map<String, String> paymentData) {return null;}
+
+    public Payment setStatus(Payment payment, String status) throws IllegalArgumentException {return null;}
+
+    public Payment getPayment(String paymentId) {return null;}
+
+    public List<Payment> getAllPayments() {return null;}
+}
