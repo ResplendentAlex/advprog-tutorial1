bd5ea7f00c57e18dd26918bcf3b3e3eea81e5355
Resplendence <142782002+ResplendentAlex@users.noreply.github.com>
2025-03-07 16:58:42 +0700

    [REFACTOR] Implement enum for PaymentMethod and PaymentStatus

---
+++ src/main/java/enums/PaymentMethod.java
@@ -0,0 +1,23 @@
+package enums;
+
+import lombok.Getter;
+
+@Getter
+public enum PaymentMethod {
+    VOUCHER("VOUCHER"),
+    COD("COD");
+
+    private final String value;
+    PaymentMethod(String value) {
+        this.value = value;
+    }
+
+    public static boolean contains(String param) {
+        for (PaymentMethod paymentMethod : PaymentMethod.values()) {
+            if (paymentMethod.getValue().equals(param)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
