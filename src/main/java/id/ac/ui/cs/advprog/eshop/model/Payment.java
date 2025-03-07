fe767842564d2f3fad026f6ea4e9216bb82f814c
Resplendence <142782002+ResplendentAlex@users.noreply.github.com>
2025-03-07 16:54:58 +0700

    [GREEN] Implement Payment

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -25,9 +25,51 @@ public class Payment {
         }
     }
 
-    public void setMethod(String method) {}
+    public void setMethod(String method) {
+        if (PaymentMethod.contains(method)) {
+            this.method = method;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
 
-    public void setStatus(String status) {}
+    public void setStatus(String status) {
+        if (PaymentStatus.contains(status)) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
 
-    public void setPaymentData(Map<String, String> paymentData) {}
+    public void setPaymentData(Map<String, String> paymentData) {
+        if (paymentData == null) {
+            throw new IllegalArgumentException();
+        }
+        this.paymentData = paymentData;
+        if (this.method.equals(PaymentMethod.VOUCHER.getValue())) {
+            if (paymentData.get("voucherCode") == null ||
+                    paymentData.get("voucherCode").length() != 16 ||
+                    !paymentData.get("voucherCode").startsWith("ESHOP")) {
+                this.setStatus(PaymentStatus.REJECTED.getValue());
+                return;
+            }
+            int digitCount = 0;
+            for (int i = 0; i < paymentData.get("voucherCode").length(); i++) {
+                if (Character.isDigit(paymentData.get("voucherCode").charAt(i))) {
+                    digitCount++;
+                }
+            }
+            if (digitCount != 8) {
+                this.setStatus(PaymentStatus.REJECTED.getValue());
+                return;
+            }
+        } else if (this.method.equals(PaymentMethod.COD.getValue())) {
+            if (paymentData.get("address") == null || paymentData.get("deliveryFee") == null ||
+                    paymentData.get("address").isEmpty() || paymentData.get("deliveryFee").isEmpty()) {
+                this.setStatus(PaymentStatus.REJECTED.getValue());
+                return;
+            }
+        }
+        this.setStatus(PaymentStatus.SUCCESS.getValue());
+    }
 }
