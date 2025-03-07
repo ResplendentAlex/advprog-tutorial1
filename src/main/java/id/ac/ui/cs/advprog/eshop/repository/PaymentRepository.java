9aff9c2482ae43f75edd722a589e608d293a7e7b
Resplendence <142782002+ResplendentAlex@users.noreply.github.com>
2025-03-07 20:10:53 +0700

    [GREEN] Add @Repository for PaymentRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -4,12 +4,14 @@ import enums.OrderStatus;
 import enums.PaymentStatus;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+@Repository
 public class PaymentRepository {
     private List<Payment> paymentData;
 
